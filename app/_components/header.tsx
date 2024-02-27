40d5ea28546df149d219b0c1c07086eb7929ec0a
ismaelczar <103972495+ismaelczar@users.noreply.github.com>
2024-02-27 18:16:06 -0300

    feat: Ajust in header component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,16 +1,15 @@
 import Link from "next/link";
-import { Bell, HelpCircle, Plus, Settings, User } from "lucide-react";
+import { Bell, HelpCircle, Plus, Settings } from "lucide-react";
 import { Separator } from "./ui/separator";
-import { Button } from "./ui/button";
 import AccountMenu from "./account-menu";
 
 const Header = () => {
   return (
     <header>
-      <div className=" flex justify-center w-full">
-        <div className="max-w-[1180px] flex items-center justify-center gap-8 h-[96px]">
-          <h1 className="text-2xl font-bold text-primary ">UPCIGA</h1>
-          <nav className="flex items-center gap-8">
+      <div className="flex justify-center">
+        <div className="max-w-[1180px] flex items-center justify-between gap-6 h-[96px] w-full">
+          <h1 className="text-3xl font-bold text-primary ">UPCIGA</h1>
+          <nav className="flex items-center gap-6">
             <Link href={''} className="flex items-center gap-0.5 text-gray-400 hover:text-white font-semibold">
               <Plus size={14} />
               Novo
@@ -19,12 +18,13 @@ const Header = () => {
             <Link href={''} className="text-gray-400 hover:text-white font-semibold">Clientes</Link>
             <Link href={''} className="text-gray-400 hover:text-white font-semibold">Chamados</Link>
             <Link href={''} className="text-gray-400 hover:text-white font-semibold">Manuais</Link>
+            <Link href={''} className="text-gray-400 hover:text-white font-semibold"></Link>
+            <Link href={''} className="text-gray-400 hover:text-white font-semibold">Manuais</Link>
           </nav>
           <div className="flex items-center gap-4">
             <HelpCircle size={20} />
             <Settings size={20} />
             <Bell size={20} />
-
             <AccountMenu />
           </div>
         </div>
