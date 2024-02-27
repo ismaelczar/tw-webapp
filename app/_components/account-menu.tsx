838767c5a4e411e7776371430068024950bafc34
ismaelczar <103972495+ismaelczar@users.noreply.github.com>
2024-02-26 22:19:55 -0300

    feat: Add initial AccountMenu component

---
+++ app/_components/account-menu.tsx
@@ -0,0 +1,30 @@
+import { LogIn, User, UserCircle } from "lucide-react";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./ui/sheet";
+import { Button } from "./ui/button";
+
+
+const AccountMenu = () => {
+  return (
+    <Sheet >
+      <SheetTrigger asChild>
+        <Button className="flex items-center gap-2">
+          <User size={16} />
+          Perfil
+        </Button>
+      </SheetTrigger>
+      <SheetContent className="p-0" >
+        <SheetHeader className="px-5 py-6 border-b border-solid">Menu</SheetHeader>
+
+        <div className="px-5 py-6">
+          <div className="flex items-center gap-2">
+            <UserCircle className="text-secondary" />
+            <h2>Olá, Faça seu login!</h2>
+          </div>
+          <Button variant="outline" className="mt-8 w-full flex items-center gap-1"> <LogIn size={14} />Faça seu login</Button>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+export default AccountMenu;
--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,31 +1,35 @@
-import { ClipboardCheck, Headset, Plus, User, UserSearch } from "lucide-react";
 import Link from "next/link";
+import { Bell, HelpCircle, Plus, Settings, User } from "lucide-react";
 import { Separator } from "./ui/separator";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
+import AccountMenu from "./account-menu";
 
 const Header = () => {
   return (
     <header>
       <div className=" flex justify-center w-full">
-        <div className="w-[1180px] flex items-center justify-center gap-8 h-[96px]">
-          <h1 className="text-2xl font-bold text-primary ">Logotipo</h1>
+        <div className="max-w-[1180px] flex items-center justify-center gap-8 h-[96px]">
+          <h1 className="text-2xl font-bold text-primary ">UPCIGA</h1>
           <nav className="flex items-center gap-8">
-            <Link href={''} className="flex items-center gap-0.5 text-gray-400 hover:text-white">
+            <Link href={''} className="flex items-center gap-0.5 text-gray-400 hover:text-white font-semibold">
               <Plus size={14} />
               Novo
             </Link>
-            <Link href={''} className="text-gray-400 hover:text-white">Ordem de Serviço</Link>
-            <Link href={''} className="text-gray-400 hover:text-white">Clientes</Link>
-            <Link href={''} className="text-gray-400 hover:text-white">Chamados</Link>
-            <Link href={''} className="text-gray-400 hover:text-white">Manuais</Link>
+            <Link href={''} className="text-gray-400 hover:text-white font-semibold">Ordem de Serviço</Link>
+            <Link href={''} className="text-gray-400 hover:text-white font-semibold">Clientes</Link>
+            <Link href={''} className="text-gray-400 hover:text-white font-semibold">Chamados</Link>
+            <Link href={''} className="text-gray-400 hover:text-white font-semibold">Manuais</Link>
           </nav>
-          <Button className="flex items-center gap-2 ">
-            <User size={16} />
-            Perfil
-          </Button>
+          <div className="flex items-center gap-4">
+            <HelpCircle size={20} />
+            <Settings size={20} />
+            <Bell size={20} />
+
+            <AccountMenu />
+          </div>
         </div>
       </div>
+
       <Separator />
     </header>
   );
