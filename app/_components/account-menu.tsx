0e4411926f8c6120b9f2a13d507f4b01359ea693
ismaelczar <103972495+ismaelczar@users.noreply.github.com>
2024-02-28 22:24:31 -0300

    feat: ajust in accountMenu component

--- app/_components/account-menu.tsx
+++ app/_components/account-menu.tsx
@@ -1,38 +1,70 @@
 'use client'
 
-import { LogIn, User, UserCircle } from "lucide-react";
+import { LogIn, LogOut, User, UserCircle } from "lucide-react";
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./ui/sheet";
 import { Button } from "./ui/button";
-import { signIn } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
+import { Avatar, AvatarImage } from "./ui/avatar";
+
 
 
 const AccountMenu = () => {
+  const { data, status } = useSession()
 
   function handleLoginClick() {
     signIn('google')
   }
 
+  function handleLogOutClick() {
+    signOut()
+  }
+
   return (
     <Sheet >
       <SheetTrigger asChild>
-        <Button className="flex items-center gap-2">
-          <User size={16} />
-          Perfil
-        </Button>
+        {data?.user ?
+          (
+            <div className="flex items-center gap-2 cursor-pointer">
+              <Avatar>
+                <AvatarImage src={data?.user.image ?? ""} />
+              </Avatar>
+              <h2 className="text-white">{data?.user.name}</h2>
+            </div>
+          )
+          : (
+            <Button className="flex items-center gap-2">
+              <User size={16} />
+              Perfil
+            </Button>
+          )
+        }
       </SheetTrigger>
+
       <SheetContent className="p-0" >
         <SheetHeader className="px-5 py-6 border-b border-solid">Menu</SheetHeader>
-
-        <div className="px-5 py-6">
-          <div className="flex items-center gap-2">
-            <UserCircle className="text-gray-500" />
-            <h2>Olá, Faça seu login!</h2>
-          </div>
-          <Button className="mt-8 w-full flex items-center gap-1 text-white" onClick={handleLoginClick}> <LogIn size={14} />Faça seu login</Button>
-        </div>
-      </SheetContent>
-    </Sheet>
+        {data?.user ?
+          (
+            <div className="px-5 py-6">
+              <div className="flex items-center gap-2">
+                <h2>Olá, {data.user.name}</h2>
+              </div>
+              <Button className="mt-8 w-full flex items-center gap-1 text-white" onClick={handleLogOutClick}> <LogOut size={14} />Sair</Button>
+            </div>
+          ) : (
+            <div className="px-5 py-6">
+              <div className="flex items-center gap-2">
+                <UserCircle className="text-gray-500" />
+                <h2>Olá, Faça seu login!</h2>
+              </div>
+              <Button className="mt-8 w-full flex items-center gap-1 text-white" onClick={handleLoginClick}> <LogIn size={14} />Faça seu login</Button>
+            </div>
+          )
+        }
+      </SheetContent >
+    </Sheet >
   );
 }
 
 export default AccountMenu;
+
+
